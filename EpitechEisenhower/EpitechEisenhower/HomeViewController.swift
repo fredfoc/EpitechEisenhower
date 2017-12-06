6815cd7c2e5aa9e56fbe98ad9902b6cbbb6ea3dc
fauquette fred <fredfocmac@gmail.com>
2017-12-06 15:09:12 +0100

    Add Task Model

--- EpitechEisenhower/EpitechEisenhower/HomeViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/HomeViewController.swift
@@ -10,25 +10,26 @@ import UIKit
 
 class HomeViewController: UICollectionViewController {
     var monTitre: String?
+    var content = [TaskModel]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = monTitre
+        content.append(TaskModel(titre: "Fred"))
+        content.append(TaskModel(titre: "Simon"))
+        content.append(TaskModel(titre: "Alex"))
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return content.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell: UICollectionViewCell!
-        if indexPath.row == 0 {
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "autreCell", for: indexPath)
-        } else {
-            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReuseId", for: indexPath)
-            let customCell = cell as? CustomCell
-            customCell?.myTitle.text = "fred"
-        }
+        cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellReuseId", for: indexPath)
+        let customCell = cell as? CustomCell
+        let tmpModel = content[indexPath.row]
+        customCell?.myTitle.text = tmpModel.titre
         return cell
     }
     
---
+++ EpitechEisenhower/EpitechEisenhower/TaskModel.swift
@@ -0,0 +1,16 @@
+//
+//  TaskModel.swift
+//  EpitechEisenhower
+//
+//  Created by fauquette fred on 6/12/17.
+//  Copyright © 2017 Epitech. All rights reserved.
+//
+
+import Foundation
+
+class TaskModel {
+    private(set) var titre: String
+    init(titre:String) {
+        self.titre = titre
+    }
+}
